36a9983d9d90e218e020836c2000892a52d1c75f
Prathmesh Chaudhari <prathmesh299@gmail.com>
2017-08-21 22:36:25 +0530

    First Commit

---
+++ Desktop/TheNextBigThing/somefile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insert Data</title>
+</head>
+<body>
+	<form name="DataEntry" action="action.php">
+		<label>Lorem Ipsum</label><input type="text" name="ID1"/>
+		<br>
+		<br>
+		<label>Lorem Ipsum</label><input type="text" name="ID2"/>
+		<br>
+		<br>
+		<label>Lorem Ipsum</label><input type="text" name="ID3"/>
+		<br>
+		<br>
+		<label>Lorem Ipsum</label><input type="text" name="ID4"/>
+	</form>
+</body>
+</html>
